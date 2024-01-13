4db0fec3d7865d122220091a1019c188361d8d24
Fernando Petrola <fernandop@sonatype.com>
2024-01-13 01:27:41 -0300

    1

---
+++ src/main/java/com/fpetrola/z80/opcodes/table/IndexerRegisterTableOpCodeGenerator.java
@@ -0,0 +1,61 @@
+package com.fpetrola.z80.opcodes.table;
+
+import static com.fpetrola.z80.registers.RegisterName.H;
+import static com.fpetrola.z80.registers.RegisterName.L;
+
+import com.fpetrola.z80.State;
+import com.fpetrola.z80.instructions.Ld;
+import com.fpetrola.z80.instructions.MemoryPlusRegister8BitReference;
+import com.fpetrola.z80.instructions.OpCode;
+import com.fpetrola.z80.instructions.OpcodeReference;
+import com.fpetrola.z80.instructions.SpyInterface;
+import com.fpetrola.z80.registers.Register;
+import com.fpetrola.z80.registers.RegisterName;
+
+public class IndexerRegisterTableOpCodeGenerator extends UnprefixedTableOpCodeGenerator {
+  private final RegisterName lowRegisterName;
+  private final RegisterName highRegisterName;
+  private final RegisterName registerName;
+
+  public IndexerRegisterTableOpCodeGenerator(State state, SpyInterface opcodesSpy, OpCode cbOpcode, OpCode ddOpcode, OpCode edOpcode, OpCode fdOpcode, RegisterName main16BitRegister, RegisterName mainHigh8BitRegister, RegisterName mainLow8BitRegister, OpcodeReference main16BitRegisterReference, RegisterName lowRegisterName, RegisterName highRegisterName, RegisterName registerName) {
+    super(state, opcodesSpy, cbOpcode, ddOpcode, edOpcode, fdOpcode, main16BitRegister, mainHigh8BitRegister, mainLow8BitRegister, main16BitRegisterReference);
+    this.lowRegisterName = lowRegisterName;
+    this.highRegisterName = highRegisterName;
+    this.registerName = registerName;
+  }
+
+  protected Ld createLd() {
+    OpcodeReference target = r[y];
+    OpcodeReference source = r[z];
+
+    if (isHL(source) || isHL(target)) {
+      source = replaceLowHigh(source);
+      target = replaceLowHigh(target);
+    }
+    return new Ld(s, target, source);
+  }
+
+  private OpcodeReference replaceLowHigh(OpcodeReference source) {
+    if (source instanceof Register) {
+      Register register = (Register) source;
+      if (register.equals(r(lowRegisterName)))
+        return r(L);
+      else if (register.equals(r(highRegisterName)))
+        return r(H);
+    }
+
+    return source;
+  }
+
+  private boolean isHL(OpcodeReference source) {
+    return source instanceof MemoryPlusRegister8BitReference;
+  }
+
+  protected Ld createLd1() {
+    OpcodeReference target = r[y];
+    if (isHL(target))
+      return new Ld(s, iRRn(registerName, false, -1), n(1));
+    else
+      return new Ld(s, target, n());
+  }
+}
--- src/main/java/com/fpetrola/z80/opcodes/table/TableBasedOpCodeDecoder.java
+++ src/main/java/com/fpetrola/z80/opcodes/table/TableBasedOpCodeDecoder.java
@@ -11,8 +11,6 @@ import static com.fpetrola.z80.registers.RegisterName.IYL;
 import static com.fpetrola.z80.registers.RegisterName.L;
 
 import com.fpetrola.z80.State;
-import com.fpetrola.z80.instructions.Ld;
-import com.fpetrola.z80.instructions.MemoryPlusRegister8BitReference;
 import com.fpetrola.z80.instructions.Nop;
 import com.fpetrola.z80.instructions.OpCode;
 import com.fpetrola.z80.instructions.OpcodeReference;
@@ -20,7 +18,6 @@ import com.fpetrola.z80.instructions.OpcodeTargets;
 import com.fpetrola.z80.instructions.SpyInterface;
 import com.fpetrola.z80.opcodes.FlipOpcodeImpl;
 import com.fpetrola.z80.opcodes.OpCodeDecoder;
-import com.fpetrola.z80.registers.Register;
 import com.fpetrola.z80.registers.RegisterName;
 
 public class TableBasedOpCodeDecoder implements OpCodeDecoder {
@@ -39,44 +36,8 @@ public class TableBasedOpCodeDecoder implements OpCodeDecoder {
   }
 
   private OpCode[] fillDDFD(State s, SpyInterface spy, RegisterName registerName, final RegisterName highRegisterName, final RegisterName lowRegisterName, OpcodeReference a) {
-    OpCode[] CBTable = new DDCBFDCBPrefixTableOpCodeGenerator(s, spy, registerName, highRegisterName, lowRegisterName, a).getOpcodesTable();
-    OpCode cbOpcode = new FlipOpcodeImpl(s, CBTable, 2, "DDFDCB", spy);
-    UnprefixedTableOpCodeGenerator ddTableOpCodeGenerator = new UnprefixedTableOpCodeGenerator(s, spy, cbOpcode, new Nop(s), new Nop(s), new Nop(s), registerName, highRegisterName, lowRegisterName, a) {
-      protected Ld createLd() {
-        OpcodeReference target = r[y];
-        OpcodeReference source = r[z];
-
-        if (isHL(source) || isHL(target)) {
-          source = replaceLowHigh(source);
-          target = replaceLowHigh(target);
-        }
-        return new Ld(s, target, source);
-      }
-
-      private OpcodeReference replaceLowHigh(OpcodeReference source) {
-        if (source instanceof Register) {
-          Register register = (Register) source;
-          if (register.equals(r(lowRegisterName)))
-            return r(L);
-          else if (register.equals(r(highRegisterName)))
-            return r(H);
-        }
-
-        return source;
-      }
-
-      private boolean isHL(OpcodeReference source) {
-        return source instanceof MemoryPlusRegister8BitReference;
-      }
-
-      protected Ld createLd1() {
-        OpcodeReference target = r[y];
-        if (isHL(target))
-          return new Ld(s, iRRn(registerName, false, -1), n(1));
-        else
-          return new Ld(s, target, n());
-      }
-    };
+    OpCode cbOpcode = new FlipOpcodeImpl(s, new DDCBFDCBPrefixTableOpCodeGenerator(s, spy, registerName, highRegisterName, lowRegisterName, a).getOpcodesTable(), 2, "DDFDCB", spy);
+    UnprefixedTableOpCodeGenerator ddTableOpCodeGenerator = new IndexerRegisterTableOpCodeGenerator(s, spy, cbOpcode, new Nop(s), new Nop(s), new Nop(s), registerName, highRegisterName, lowRegisterName, a, lowRegisterName, highRegisterName, registerName);
     return ddTableOpCodeGenerator.getOpcodesTable();
   }
 
