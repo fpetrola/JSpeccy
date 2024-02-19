402fb816ead909e5be54b5ddfe7adb3b68f9e40c
Fernando Petrola <fernandop@sonatype.com>
2024-02-19 01:42:12 -0300

    instructions improvements

--- src/main/java/com/fpetrola/z80/registers/Register.java
+++ src/main/java/com/fpetrola/z80/registers/Register.java
@@ -2,24 +2,9 @@ package com.fpetrola.z80.registers;
 
 import com.fpetrola.z80.opcodes.references.OpcodeReference;
 
-/**
- * 8-bit or 16-bit register that can be redden and written.
- *
- * @author fpreto
- */
 public interface Register<T> extends OpcodeReference<T> {
-
-    /**
-     * Read the data from register
-     */
     T read();
-
-    /**
-     * Write data to register
-     */
     void write(T value);
-
-
     void increment();
     void decrement();
     RegisterName getName();
--- src/main/java/com/fpetrola/z80/registers/RegisterPair.java
+++ src/main/java/com/fpetrola/z80/registers/RegisterPair.java
@@ -1,8 +1,6 @@
 package com.fpetrola.z80.registers;
 
 public interface RegisterPair<T> extends Register<T> {
-
     Register<T> getHigh();
-
     Register<T> getLow();
 }
