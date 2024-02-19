da4550dcd8fdf201b8e311ee8dc2a9d6bba610df
Fernando Petrola <fernandop@sonatype.com>
2024-02-19 01:44:16 -0300

    instructions improvements

--- src/main/java/com/fpetrola/z80/registers/Register.java
+++ src/main/java/com/fpetrola/z80/registers/Register.java
@@ -3,8 +3,6 @@ package com.fpetrola.z80.registers;
 import com.fpetrola.z80.opcodes.references.OpcodeReference;
 
 public interface Register<T> extends OpcodeReference<T> {
-    T read();
-    void write(T value);
     void increment();
     void decrement();
     RegisterName getName();
