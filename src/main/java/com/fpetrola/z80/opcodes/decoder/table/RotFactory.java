2d05548433d31c6b306abba125e7c41393f19e13
Fernando Petrola <fernandop@sonatype.com>
2024-02-20 18:53:14 -0300

    different hierarchies for Opcode References

--- src/main/java/com/fpetrola/z80/opcodes/decoder/table/RotFactory.java
+++ src/main/java/com/fpetrola/z80/opcodes/decoder/table/RotFactory.java
@@ -1,6 +1,7 @@
 package com.fpetrola.z80.opcodes.decoder.table;
 
 import com.fpetrola.z80.instructions.base.Instruction;
+import com.fpetrola.z80.opcodes.references.ImmutableOpcodeReference;
 import com.fpetrola.z80.opcodes.references.OpcodeReference;
 
 public interface RotFactory {
--- src/main/java/com/fpetrola/z80/registers/Register.java
+++ src/main/java/com/fpetrola/z80/registers/Register.java
@@ -3,7 +3,7 @@ package com.fpetrola.z80.registers;
 import com.fpetrola.z80.opcodes.references.ImmutableOpcodeReference;
 import com.fpetrola.z80.opcodes.references.OpcodeReference;
 
-public interface Register<T> extends OpcodeReference<T>, ImmutableOpcodeReference<T> {
+public interface Register<T> extends OpcodeReference<T> {
     void increment();
     void decrement();
     RegisterName getName();
