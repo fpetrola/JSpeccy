ba411a48dfae37053b0195289d7c5c085db21170
Fernando Petrola <fernandop@sonatype.com>
2024-03-08 19:42:03 -0300

    refactoring TransformerVisitor

--- src/test/java/com/fpetrola/z80/instructions/transformations/TransformerVisitor.java
+++ src/test/java/com/fpetrola/z80/instructions/transformations/TransformerVisitor.java
@@ -1,8 +1,6 @@
 package com.fpetrola.z80.instructions.transformations;
 
 import com.fpetrola.z80.blocks.DummyInstructionVisitor;
-import com.fpetrola.z80.instructions.DummyImmutableOpcodeReference;
-import com.fpetrola.z80.instructions.base.Instruction;
 import com.fpetrola.z80.instructions.base.InstructionVisitor;
 import com.fpetrola.z80.instructions.base.TargetInstruction;
 import com.fpetrola.z80.instructions.base.TargetSourceInstruction;
@@ -10,51 +8,15 @@ import com.fpetrola.z80.opcodes.references.ImmutableOpcodeReference;
 import com.fpetrola.z80.opcodes.references.IndirectMemory8BitReference;
 import com.fpetrola.z80.opcodes.references.OpcodeReference;
 import com.fpetrola.z80.opcodes.references.WordNumber;
-import com.fpetrola.z80.registers.Composed16BitRegister;
 import com.fpetrola.z80.registers.Register;
-import com.fpetrola.z80.registers.RegisterPair;
-import org.apache.commons.collections4.MultiValuedMap;
-import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 @SuppressWarnings("ALL")
-public class TransformerVisitor<T extends WordNumber> extends DummyInstructionVisitor<T> implements InstructionVisitor<T> {
-  private MultiValuedMap<String, String> names = new HashSetValuedHashMap<>();
-  protected Map<Register, Register> targets = new HashMap<>();
+public class TransformerVisitor<T extends WordNumber> extends DummyInstructionVisitor<T> {
+  private final VirtualRegisterFactory<T> virtualRegisterFactory;
 
   public <T extends WordNumber> TransformerVisitor() {
-    this.targets = targets;
-  }
-
-  public <T extends WordNumber> Register createVirtualRegister(Register register, Instruction<T> targetInstruction, ImmutableOpcodeReference<T> targetRegister, boolean[] executing) {
-    Register virtualRegister = new VirtualPlain8BitRegister(createVirtualRegisterName(register.getName()), executing, targetInstruction, targetRegister);
-    targets.put(register, virtualRegister);
-    return virtualRegister;
-  }
-
-  public <T extends WordNumber> Register create16VirtualRegister(Instruction<T> targetInstruction, RegisterPair registerPair, boolean indirect) {
-    Register high = registerPair.getHigh();
-    Register low = registerPair.getLow();
-    if (!indirect) {
-      boolean[] executing = new boolean[1];
-      createVirtualRegister(high, targetInstruction, getTargetRegister(high), executing);
-      createVirtualRegister(low, targetInstruction, getTargetRegister(low), executing);
-    }
-    Composed16BitRegister<WordNumber> virtualRegister = new Composed16BitRegister<>(createVirtualRegisterName(high.getName() + low.getName()), targets.get(high), targets.get(low));
-    targets.put(registerPair, virtualRegister);
-    return virtualRegister;
-  }
-
-  private <T extends WordNumber> DummyImmutableOpcodeReference<T> getTargetRegister(Register register) {
-    Register lastVirtualRegister = targets.get(register);
-    return new DummyImmutableOpcodeReference<T>() {
-      public T read() {
-        return (T) lastVirtualRegister.read();
-      }
-    };
+    virtualRegisterFactory = new VirtualRegisterFactory();
   }
 
   @Override
@@ -62,36 +24,22 @@ public class TransformerVisitor<T extends WordNumber> extends DummyInstructionVi
     if (target instanceof IndirectMemory8BitReference indirectMemory8BitReference) {
       OpcodeReference target1 = (OpcodeReference) indirectMemory8BitReference.target;
       if (target1 instanceof Register register) {
-        indirectMemory8BitReference.target = createVirtualRegister(targetInstruction, register, true);
+        indirectMemory8BitReference.target = virtualRegisterFactory.createVirtualRegister(targetInstruction, register, true);
       }
     } else if (target instanceof Register register) {
-      targetInstruction.setTarget(createVirtualRegister(targetInstruction, register, false));
+      targetInstruction.setTarget(virtualRegisterFactory.createVirtualRegister(targetInstruction, register, false));
     }
-  }
-
-  private Register createVirtualRegister(TargetInstruction targetInstruction, Register register, boolean indirect) {
-    if (register instanceof RegisterPair registerPair)
-      return create16VirtualRegister(targetInstruction, registerPair, indirect);
-    else
-      return createVirtualRegister(register, targetInstruction, targets.get(register), new boolean[1]);
   }
 
   @Override
   public void visitingSource(ImmutableOpcodeReference source, TargetSourceInstruction targetSourceInstruction) {
     if (source instanceof Register register) {
-      Register virtual = targets.get(register);
+      Register virtual = virtualRegisterFactory.getVirtualRegisterFor(register);
 
-      if (virtual == null )
-        virtual = createVirtualRegister(null, register, true);
+      if (virtual == null)
+        virtual = virtualRegisterFactory.createVirtualRegister(null, register, true);
 
       targetSourceInstruction.setSource(virtual);
     }
   }
-
-  private String createVirtualRegisterName(String name) {
-    String s = name + "_v" + names.get(name).size();
-    names.put(name, s);
-    return s;
-  }
-
 }
---
+++ src/test/java/com/fpetrola/z80/instructions/transformations/VirtualRegisterFactory.java
@@ -0,0 +1,69 @@
+package com.fpetrola.z80.instructions.transformations;
+
+import com.fpetrola.z80.instructions.DummyImmutableOpcodeReference;
+import com.fpetrola.z80.instructions.base.Instruction;
+import com.fpetrola.z80.instructions.base.TargetInstruction;
+import com.fpetrola.z80.opcodes.references.ImmutableOpcodeReference;
+import com.fpetrola.z80.opcodes.references.WordNumber;
+import com.fpetrola.z80.registers.Composed16BitRegister;
+import com.fpetrola.z80.registers.Register;
+import com.fpetrola.z80.registers.RegisterPair;
+import org.apache.commons.collections4.MultiValuedMap;
+import org.apache.commons.collections4.multimap.HashSetValuedHashMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class VirtualRegisterFactory<T extends WordNumber> {
+  private Map<Register, Register> targets = new HashMap<>();
+  private MultiValuedMap<String, String> names = new HashSetValuedHashMap<>();
+
+  public VirtualRegisterFactory() {
+    this.targets = targets;
+  }
+
+  public Register createVirtualRegister(TargetInstruction targetInstruction, Register register, boolean indirect) {
+    if (register instanceof RegisterPair registerPair)
+      return create16VirtualRegister(targetInstruction, registerPair, indirect);
+    else
+      return createVirtualRegister(register, targetInstruction, targets.get(register), new boolean[1]);
+  }
+
+  public Register getVirtualRegisterFor(Register register) {
+    return targets.get(register);
+  }
+
+  private <T extends WordNumber> Register createVirtualRegister(Register register, Instruction<T> targetInstruction, ImmutableOpcodeReference<T> targetRegister, boolean[] executing) {
+    Register virtualRegister = new VirtualPlain8BitRegister(createVirtualRegisterName(register.getName()), executing, targetInstruction, targetRegister);
+    targets.put(register, virtualRegister);
+    return virtualRegister;
+  }
+
+  private <T extends WordNumber> Register create16VirtualRegister(Instruction<T> targetInstruction, RegisterPair registerPair, boolean indirect) {
+    Register high = registerPair.getHigh();
+    Register low = registerPair.getLow();
+    if (!indirect) {
+      boolean[] executing = new boolean[1];
+      createVirtualRegister(high, targetInstruction, getTargetRegister(high), executing);
+      createVirtualRegister(low, targetInstruction, getTargetRegister(low), executing);
+    }
+    Composed16BitRegister<WordNumber> virtualRegister = new Composed16BitRegister<>(createVirtualRegisterName(high.getName() + low.getName()), targets.get(high), targets.get(low));
+    targets.put(registerPair, virtualRegister);
+    return virtualRegister;
+  }
+
+  private <T extends WordNumber> DummyImmutableOpcodeReference<T> getTargetRegister(Register register) {
+    Register lastVirtualRegister = targets.get(register);
+    return new DummyImmutableOpcodeReference<T>() {
+      public T read() {
+        return (T) lastVirtualRegister.read();
+      }
+    };
+  }
+
+  private String createVirtualRegisterName(String name) {
+    String s = name + "_v" + names.get(name).size();
+    names.put(name, s);
+    return s;
+  }
+}
