19e8905560e1b87e3b68eab331cbbd600f97b876
Fernando Petrola <fernandop@sonatype.com>
2024-03-13 01:57:59 -0300

    RLD

--- src/main/java/com/fpetrola/z80/instructions/RLD.java
+++ src/main/java/com/fpetrola/z80/instructions/RLD.java
@@ -6,12 +6,14 @@ import com.fpetrola.z80.opcodes.references.WordNumber;
 import com.fpetrola.z80.registers.Register;
 import com.fpetrola.z80.registers.flag.FlagRegister;
 
+import static com.fpetrola.z80.opcodes.references.WordNumber.createValue;
+
 public class RLD<T extends WordNumber> extends AbstractInstruction<T> {
-  private final Register<T> a;
-  private final Register<T> hl;
-  private final FlagRegister<T> flag;
-  private final Register<T> r;
-  private final Memory<T> memory;
+  protected final Register<T> a;
+  protected final Register<T> hl;
+  protected final FlagRegister<T> flag;
+  protected final Register<T> r;
+  protected final Memory<T> memory;
 
   public RLD(Register<T> a, Register<T> hl, FlagRegister<T> flag, Register<T> r, Memory<T> memory) {
     this.a = a;
@@ -23,20 +25,32 @@ public class RLD<T extends WordNumber> extends AbstractInstruction<T> {
 
   public int execute() {
     int reg_A = a.read().intValue();
-
-    int temp = memory.read(hl.read()).intValue();
     int nibble1 = (reg_A & 0x00F0) >> 4;
     int nibble2 = reg_A & 0x000F;
+
+    int temp = memory.read(hl.read()).intValue();
     int nibble3 = (temp & 0x00F0) >> 4;
     int nibble4 = temp & 0x000F;
-    reg_A = (nibble1 << 4) | nibble3;
-    temp = (nibble4 << 4) | nibble2;
 
-    memory.write(hl.read(), WordNumber.createValue(temp));
-    flag.RLD(WordNumber.createValue(reg_A));
-    
-    a.write(WordNumber.createValue(reg_A));
+    memory.write(hl.read(), createValue(getTemp1(nibble2, nibble3, nibble4)));
+    T value = createValue(getRegA1(nibble1, nibble4, nibble3));
+
+    executeAlu(value);
+
+    a.write(value);
 
     return 1;
+  }
+
+  protected void executeAlu(T value) {
+    flag.RLD(value);
+  }
+
+  protected int getTemp1(int nibble2, int nibble3, int nibble4) {
+    return (nibble4 << 4) | nibble2;
+  }
+
+  protected int getRegA1(int nibble1, int nibble4, int nibble3) {
+    return (nibble1 << 4) | nibble3;
   }
 }
--- src/main/java/com/fpetrola/z80/instructions/RRD.java
+++ src/main/java/com/fpetrola/z80/instructions/RRD.java
@@ -1,42 +1,24 @@
 package com.fpetrola.z80.instructions;
 
-import com.fpetrola.z80.instructions.base.AbstractInstruction;
 import com.fpetrola.z80.mmu.Memory;
 import com.fpetrola.z80.opcodes.references.WordNumber;
 import com.fpetrola.z80.registers.Register;
 import com.fpetrola.z80.registers.flag.FlagRegister;
 
-public class RRD<T extends WordNumber> extends AbstractInstruction<T> {
-  private final Register<T> a;
-  private final Register<T> hl;
-  private final Register<T> r;
-  private final FlagRegister<T> flag;
-  private final Memory<T> memory;
-
+public class RRD<T extends WordNumber> extends RLD<T> {
   public RRD(Register<T> a, Register<T> hl, Register<T> r, FlagRegister<T> flag, Memory<T> memory) {
-    this.a = a;
-    this.hl = hl;
-    this.r = r;
-    this.flag = flag;
-    this.memory = memory;
+    super(a, hl, flag, r, memory);
   }
 
-  public int execute() {
-    int reg_A = a.read().intValue();
+  protected void executeAlu(T value) {
+    flag.RRD(value);
+  }
 
-    int temp = memory.read(hl.read()).intValue();
-    int nibble1 = (reg_A & 0x00F0) >> 4;
-    int nibble2 = reg_A & 0x000F;
-    int nibble3 = (temp & 0x00F0) >> 4;
-    int nibble4 = temp & 0x000F;
-    reg_A = (nibble1 << 4) | nibble4;
-    temp = (nibble2 << 4) | nibble3;
+  protected int getTemp1(int nibble2, int nibble3, int nibble4) {
+    return (nibble2 << 4) | nibble3;
+  }
 
-    memory.write(hl.read(), WordNumber.createValue(temp));
-    flag.RRD(WordNumber.createValue(reg_A));
-
-    a.write(WordNumber.createValue(reg_A));
-
-    return 1;
+  protected int getRegA1(int nibble1, int nibble4, int nibble3) {
+    return (nibble1 << 4) | nibble4;
   }
 }
