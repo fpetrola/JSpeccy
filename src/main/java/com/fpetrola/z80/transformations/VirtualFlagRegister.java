1a43655741331ea13ded9eee46d60e6a034822d5
Fernando Petrola <fernandop@sonatype.com>
2024-03-20 11:45:38 -0300

    refactoring

--- src/main/java/com/fpetrola/z80/transformations/VirtualFlagRegister.java
+++ src/main/java/com/fpetrola/z80/transformations/VirtualFlagRegister.java
@@ -13,8 +13,8 @@ import static com.fpetrola.z80.registers.flag.FlagProxyFactory.adaptArgs;
 public class VirtualFlagRegister<T extends WordNumber> extends Virtual8BitsRegister<T> implements FlagRegisterDelegate<T> {
   FlagRegister<T> delegate;
 
-  public VirtualFlagRegister(InstructionExecutor instructionExecutor, String name, Instruction<T> instruction, VirtualRegister<T> lastValueSupplier, VirtualFetcher<T> virtualFetcher) {
-    super(instructionExecutor, name, instruction, lastValueSupplier, virtualFetcher);
+  public VirtualFlagRegister(InstructionExecutor instructionExecutor, String name, Instruction<T> instruction, VirtualRegister<T> previousVersion, VirtualFetcher<T> virtualFetcher) {
+    super(instructionExecutor, name, instruction, previousVersion, virtualFetcher);
 
     TableFlagRegister tableFlagRegister = new TableFlagRegister("virtualFlag");
     delegate = FlagProxyFactory.createProxy((proxy, method, args) -> {
